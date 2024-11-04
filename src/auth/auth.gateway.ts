5eae1fc11892aee2fb5429796c590bde10ff1e60
eliyya <tecno_eli@hotmail.com>
2024-11-03 20:21:03 -0600

    types

--- src/auth/auth.gateway.ts
+++ src/auth/auth.gateway.ts
@@ -4,84 +4,13 @@ import {
     SubscribeMessage,
     OnGatewayConnection,
     OnGatewayDisconnect,
+    MessageBody,
+    ConnectedSocket,
 } from '@nestjs/websockets'
-import { Server, Socket } from 'socket.io'
+import { Server } from 'socket.io'
+import { EventsEmitMap, EventsListenerMap, SocketClient, User } from './types'
 
-type SocketClient = Socket<EventsEmitClientMap, EventsListenerClientMap>
-interface User {
-    id: string
-    login: string
-}
-
-export interface GithubSuccesResponse {
-    login: string
-    id: number
-    node_id: string
-    avatar_url: string
-    gravatar_id: string
-    url: string
-    html_url: string
-    followers_url: string
-    following_url: string
-    gists_url: string
-    starred_url: string
-    subscriptions_url: string
-    organizations_url: string
-    repos_url: string
-    events_url: string
-    received_events_url: string
-    type: string
-    user_view_type: string
-    site_admin: boolean
-    name: string
-    company: any
-    blog: string
-    location: string
-    email: any
-    hireable: any
-    bio: string
-    twitter_username: string
-    public_repos: number
-    public_gists: number
-    followers: number
-    following: number
-    created_at: string
-    updated_at: string
-}
-
-export interface Message {
-    id: string
-    username: string
-    avatar?: string
-    content: string
-    createdAt: string
-}
-
-interface EventsListenerClientMap {
-    // [event: string]: (...args: any[]) => void
-    authSuccess: (data: { message: string; user: GithubSuccesResponse }) => any
-    authError: (data: { message: string }) => any
-    message(data: { username: string; message: string }): any
-}
-interface EventsEmitClientMap {
-    // [event: string]: (...args: any[]) => void
-    auth: (token: string) => any
-    sendMessage: (message: Message) => any
-}
-interface EventsListenerMap {
-    // [event: string]: (...args: any[]) => void
-    t: (d: any) => any
-}
-interface EventsEmitMap {
-    // [event: string]: (...args: any[]) => void
-    userConnected: (data: { username: string }) => any
-}
-
-@WebSocketGateway({
-    cors: {
-        origin: '*',
-    },
-})
+@WebSocketGateway({ cors: { origin: '*', credentials: true } })
 export class AuthGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer() server: Server<EventsListenerMap, EventsEmitMap>
     private users: Map<string, User> = new Map()
@@ -96,22 +25,19 @@ export class AuthGateway implements OnGatewayConnection, OnGatewayDisconnect {
     }
 
     @SubscribeMessage('auth')
-    async handleAuth(client: SocketClient, token: string) {
+    async handleAuth(
+        @ConnectedSocket() client: SocketClient,
+        @MessageBody() token: string,
+    ) {
         console.log(`Client trying autenicate: ${client.id}`)
 
         try {
-            // Validar el token de GitHub utilizando fetch
             const response = await fetch('https://api.github.com/user', {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
             })
-
-            // Verificar si la respuesta es exitosa
-            if (!response.ok) {
-                throw new Error('Authentication failed')
-            }
-
+            if (!response.ok) throw new Error('Authentication failed')
             const userData = await response.json()
 
             // Agregar el usuario a la lista de usuarios conectados
@@ -132,7 +58,10 @@ export class AuthGateway implements OnGatewayConnection, OnGatewayDisconnect {
     }
 
     @SubscribeMessage('sendMessage')
-    handleMessage(client: SocketClient, message: string) {
+    handleMessage(
+        @ConnectedSocket() client: SocketClient,
+        @MessageBody() message: string,
+    ) {
         const user = this.users.get(client.id)
         console.log({ username: user.login, message })
         if (user) {
---
+++ src/auth/types.ts
@@ -0,0 +1,71 @@
+import { Socket } from 'socket.io'
+
+export type SocketClient = Socket<EventsEmitClientMap, EventsListenerClientMap>
+export interface User {
+    id: string
+    login: string
+}
+
+export interface GithubSuccesResponse {
+    login: string
+    id: number
+    node_id: string
+    avatar_url: string
+    gravatar_id: string
+    url: string
+    html_url: string
+    followers_url: string
+    following_url: string
+    gists_url: string
+    starred_url: string
+    subscriptions_url: string
+    organizations_url: string
+    repos_url: string
+    events_url: string
+    received_events_url: string
+    type: string
+    user_view_type: string
+    site_admin: boolean
+    name: string
+    company: any
+    blog: string
+    location: string
+    email: any
+    hireable: any
+    bio: string
+    twitter_username: string
+    public_repos: number
+    public_gists: number
+    followers: number
+    following: number
+    created_at: string
+    updated_at: string
+}
+
+export interface Message {
+    id: string
+    username: string
+    avatar?: string
+    content: string
+    createdAt: string
+}
+
+export interface EventsListenerClientMap {
+    // [event: string]: (...args: any[]) => void
+    authSuccess: (data: { message: string; user: GithubSuccesResponse }) => any
+    authError: (data: { message: string }) => any
+    message(data: { username: string; message: string }): any
+}
+export interface EventsEmitClientMap {
+    // [event: string]: (...args: any[]) => void
+    auth: (token: string) => any
+    sendMessage: (message: Message) => any
+}
+export interface EventsListenerMap {
+    // [event: string]: (...args: any[]) => void
+    t: (d: any) => any
+}
+export interface EventsEmitMap {
+    // [event: string]: (...args: any[]) => void
+    userConnected: (data: { username: string }) => any
+}
