e5dd067e7643a93ad62ea904e9a3f63e7366ed74
eliyya <tecno_eli@hotmail.com>
2024-11-05 03:50:08 -0600

    auth fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,37 +1,53 @@
 // src/token/token.controller.ts
-import { Controller, Get, Headers, HttpStatus, Res } from '@nestjs/common'
+import {
+    Body,
+    Controller,
+    Headers,
+    HttpStatus,
+    Post,
+    Res,
+} from '@nestjs/common'
 import { Response } from 'express'
 
 @Controller('token')
 export class AuthController {
-    @Get('validate')
-    async validateToken(
-        @Headers('authorization') authHeader: string,
-        @Res() res: Response,
-    ) {
+    @Post('validate')
+    async validateToken(@Body('code') code: string, @Res() res: Response) {
         // Aquí puedes hacer la validación del token si es necesario
-        if (authHeader) {
-            const token = authHeader.replace('Bearer ', '')
-            // Realiza la lógica para validar el token aquí
-            console.log('Token recibido:', token)
+        if (code) {
             try {
-                const response = await fetch('https://api.github.com/user', {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
+                const tokenData = await fetch(
+                    'https://github.com/login/oauth/access_token',
+                    {
+                        method: 'POST',
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            client_id: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
+                            client_secret: process.env.GITHUB_CLIENT_SECRET,
+                            code,
+                        }),
                     },
-                })
-                if (!response.ok) throw new Error('Authentication failed')
-                const userData = await response.json()
-                return res.status(HttpStatus.OK).json(userData) // Devuelve 200 OK
+                ).then(r => r.json())
+
+                if (!tokenData.access_token) {
+                    console.log('No TOKEN', tokenData)
+                    return res.status(HttpStatus.OK).json(tokenData)
+                } else
+                    return res
+                        .status(HttpStatus.UNAUTHORIZED)
+                        .json({ message: 'Invalid code' })
             } catch (error) {
                 console.error('Authentication error:', error)
                 return res
                     .status(HttpStatus.UNAUTHORIZED)
-                    .json({ message: 'Invalid token' })
+                    .json({ message: 'Invalid code' })
             }
         }
         return res
             .status(HttpStatus.UNAUTHORIZED)
-            .json({ message: 'No token provided' })
+            .json({ message: 'No code provided' })
     }
 }
