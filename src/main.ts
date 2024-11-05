71dcd7b065fbb1823a592e5b1617edcdf2e09ebe
eliyya <tecno_eli@hotmail.com>
2024-11-05 01:00:34 -0600

    remove ssl

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,7 @@ import { AppModule } from './app.module'
 import { readFile } from 'fs/promises'
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
-        httpsOptions:
-            process.env.NODE_ENV !== 'production' ?
-                undefined
-            :   {
-                    key: await readFile('./selfsigned.key'),
-                    cert: await readFile('./selfsigned.crt'),
-                },
-    })
+    const app = await NestFactory.create(AppModule)
     await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
